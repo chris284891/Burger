f67b64249b1d1bc58e52497b445b31fdab2524a2
chris284891 <chris284891@gmail.com>
2019-08-23 16:47:59 -0400

    completed server.js and tested

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+drop database burgers_db;
+create database burgers_db;
+use burgers_db;
+create table burgers (
+	id int not null auto_increment,
+    burger_name varchar(50) not null,
+    devoured boolean not null,
+    primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Blue Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Angus", true);
