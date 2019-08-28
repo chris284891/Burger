c2de5b7830b1deac0731c84287ceed3d58f2f9b2
chris284891 <chris284891@gmail.com>
2019-08-28 14:19:55 -0400

    changed connection config to match JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: process.env.JAWSDB_URL,
+		database: "b9s1dwhimzc7wzmh"
+	});
+};
 
 connection.connect(function(err) {
   if (err) {
